6e3cd7307c74f697c32bc73ff7d3edaa8423b92a
Undavalli Alekhya Krishna <vtu12706@veltech.edu.in>
2024-06-26 19:29:28 +0530

    App.js

--- App.js
+++ App.js
@@ -1,36 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import SignupPopup from './components/SignupPopup';
 import HomePage from './components/HomePage';
 import BookAppointmentPage from './components/BookAppointmentPage';
-import './App.css'; // You can import CSS for styling here
+import AppointmentsPage from './components/AppointmentsPage';
 
-const App = () => {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1>Clinic Appointment System</h1>
-          <nav>
-            <ul className="nav-links">
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/book-appointment">Book Appointment</Link></li>
-              {/* Add more navigation links as needed */}
-            </ul>
-          </nav>
-        </header>
-        <main className="App-main">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/book-appointment" element={<BookAppointmentPage />} />
-            {/* Add more routes as needed */}
-          </Routes>
-        </main>
-        <footer className="App-footer">
-          <p>&copy; {new Date().getFullYear()} Clinic Appointment System. All rights reserved.</p>
-        </footer>
-      </div>
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPopup />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/book-appointment" element={<BookAppointmentPage />} />
+      <Route path="/appointments" element={<AppointmentsPage />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
